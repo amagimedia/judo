2916482032c9ba75279ee067813a21c141d4ab03
ajiththnn <ajithnn20@gmail.com>
2018-10-11 15:25:33 +0530

    fix subscribe script to give greater than last msg only

--- scripts/scripts.go
+++ scripts/scripts.go
@@ -2,10 +2,10 @@ package scripts
 
 const (
 	XPUBLISHSHA   = "19139a75c1a4ac8df8887010e7e4d19033fcc747"
-	XSUBSCRIBESHA = "77eadb764b2789aca2d674b121a383bbce50be33"
+	XSUBSCRIBESHA = "bafcf505307208399d76636af3b73f57b75b7407"
 )
 
 var SHAtoCode = map[string]string{
 	XPUBLISHSHA:   "local topic = ARGV[1];local ts = ARGV[3];local msg = ts .. '|' .. ARGV[2];redis.call('PUBLISH', topic, msg);redis.call('ZADD', topic, ts, msg);return msg;",
-	XSUBSCRIBESHA: "local topic = ARGV[1];local from = ARGV[2];return redis.call('ZRANGEBYSCORE', topic, from, '+inf');",
+	XSUBSCRIBESHA: "local topic = ARGV[1];local from = '(' .. ARGV[2];return redis.call('ZRANGEBYSCORE', topic, from, '+inf');",
 }
