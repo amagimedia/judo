edbcb405a719a548e0023f8fe3f96804f7a3bec5
ajiththnn <ajithnn20@gmail.com>
2018-10-11 14:59:19 +0530

    handle subscribe timestamp differently

--- scripts/scripts.go
+++ scripts/scripts.go
@@ -2,10 +2,10 @@ package scripts
 
 const (
 	XPUBLISHSHA   = "19139a75c1a4ac8df8887010e7e4d19033fcc747"
-	XSUBSCRIBESHA = "7a888fa9085114a6f4a8f3074cda494b564eb494"
+	XSUBSCRIBESHA = "77eadb764b2789aca2d674b121a383bbce50be33"
 )
 
 var SHAtoCode = map[string]string{
 	XPUBLISHSHA:   "local topic = ARGV[1];local ts = ARGV[3];local msg = ts .. '|' .. ARGV[2];redis.call('PUBLISH', topic, msg);redis.call('ZADD', topic, ts, msg);return msg;",
-	XSUBSCRIBESHA: "local topic = ARGV[1];local from = ARGV[2];local to = ARGV[3];return redis.call('ZRANGEBYSCORE', topic, from, to);",
+	XSUBSCRIBESHA: "local topic = ARGV[1];local from = ARGV[2];return redis.call('ZRANGEBYSCORE', topic, from, '+inf');",
 }
